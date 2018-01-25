b665dee77823cd4ff2cd044c7981817fed4e8df2
Roman <my-email@email.com>
2018-01-25 22:34:00 +0200

    lesson

---
+++ HttpAsyncAwait/Директивы препроцессора/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Директивы_препроцессора
+{
+    class Program
+    {
+        public static int x;
+        static void Main(string[] args)
+        {
+#if DEBUG
+            Console.WriteLine("Отладка запущена");
+            for ( int i =0; i<=10; i++)
+            {
+                x += i;
+                Console.WriteLine($"x= {x}");
+            }
+#else
+            Console.WriteLine("Отладка не запущена");
+#endif
+            Console.ReadKey();
+        }
+    }
+}
