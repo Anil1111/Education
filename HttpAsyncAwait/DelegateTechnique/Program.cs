061a097c677a897b5322f6eb365723ea3a21f06f
Roman <my-email@email.com>
2018-03-31 15:48:26 +0300

    delegate technique added

---
+++ HttpAsyncAwait/DelegateTechnique/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+//Техника делегирования.
+namespace DelegateTechnique
+{
+    class A //сапожник
+    {
+        public string DoSomething(string shoes)
+        {
+            //Console.WriteLine("Отремонтируй ботинки");
+            return shoes + "|||||||||";
+        }
+    }
+
+    class B //девушка
+    {
+        public string DoSomething(string shoes)
+        {
+            A a = new A(); //девушка создвет сапожника и говорит ему то же самое , что и я сказал ей.
+            return a.DoSomething(shoes);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            B b = new B();
+            string renovatredShoes = b.DoSomething("shoes");
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
