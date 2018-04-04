4941d82628210375f8a4f460db8be74c0fd0e702
Roman <my-email@email.com>
2018-04-05 00:14:16 +0300

    how to lesson 7 finished

---
+++ HttpAsyncAwait/007_DateTime/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Now;
+            DateTime someData= new DateTime(2018, 8,9); // есть еще много перегрузок этого конструктора.
+            TimeSpan left = someData - today;
+
+            Console.WriteLine($"Осталось {left} дней.");
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HttpAsyncAwait/007_DateTime_format/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _007_DateTime_format
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime now = DateTime.Now;
+
+            Console.WriteLine($"Дата месяц(прописью) год {now:D}");
+            Console.WriteLine($"Дата.месяц.год {now:d}");
+            Console.WriteLine($"Дата месяц(прописью) год время (чч:мм:сс) {now:F}");
+            Console.WriteLine($"Дата месяц(прописью) год время (чч:мм) {now:f}");
+            Console.WriteLine($"Дата.месяц.год время (чч:мм:cc) {now:G}");
+            Console.WriteLine($"Дата.месяц.год время (чч:мм) {now:g}");
+            Console.WriteLine($"Текущий месяц и дата {now:M}");
+            Console.WriteLine($"Текущий месяц и год {now:Y}");
+            Console.WriteLine($"Время(чч:мм:сс) {now:T}");
+            Console.WriteLine($"Время(чч:мм) {now:t}");
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
