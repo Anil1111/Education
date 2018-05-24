61cd33638324ffdc95802abdd04cfa8e199ea778
Roman <poma.borodavka@gmail.com>
2018-05-24 23:16:05 +0300

    added some clarification info for delegates

--- HttpAsyncAwait/009_Delegates/Program.cs
+++ HttpAsyncAwait/009_Delegates/Program.cs
@@ -7,14 +7,21 @@ using System.Threading.Tasks;
 namespace _009_Delegates
 {
     public delegate void MyDelegate();
+
+    public delegate int MyDelegateInt();
     class Program
     {
         static void Main(string[] args)
         {
             MyDelegate myDelegate;
+            int x= 1;
             myDelegate = delegate { Console.WriteLine("first"); }; //Лямбда метод
             myDelegate += () => { Console.WriteLine("Second"); }; //Лямбда оператор
             myDelegate += () =>  Console.WriteLine("Third");      //Лямбда выражение
+
+            MyDelegateInt myDelegateInt;
+            myDelegateInt = () => x++; //выглядит точно так же как и сверху, но ПО СИГНАТУРЕ ДЕЛЕГАТА ВИДНО ЧТО 
+            //возвращаемое значение int, а у MyDelegate - void. 
 
             myDelegate();
 
