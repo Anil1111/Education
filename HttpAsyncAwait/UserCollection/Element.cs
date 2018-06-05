0e36ca40fcba1d18ac5d24a69a147c39cb1a35ac
Roman <poma.borodavka@gmail.com>
2018-06-06 00:03:45 +0300

    Started 'collection' lesson

---
+++ HttpAsyncAwait/UserCollection/Element.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCollection
+{
+    // Экземпляры этого класса будет содержать коллекция UserCollection
+    public class Element
+    {
+        //Поля.
+        private string name;
+        private int field1;
+        private int field2;
+
+        public string Name { get => name; set => name = value; }
+        public int Field1 { get => field1; set => field1 = value; }
+        public int Field2 { get => field2; set => field2 = value; }
+    }
+}
---
+++ HttpAsyncAwait/UserCollection/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCollection
+{
+    //Коллекция- набор однотипных(в большинстве случаев) елементов. Если элементы разнотипные - то коллекция ГЕТЕРОГЕННАЯ (ArrayList)
+    //https://ru.wikipedia.org/wiki/%D0%9A%D0%BE%D0%BB%D0%BB%D0%B5%D0%BA%D1%86%D0%B8%D1%8F_(%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%B8%D0%B5)
+    //Банк - много обслуживающих механизмов - итераторов(просто СЧИТАЮТ деньги). Пример итератора - банкомат.
+    //Паттерн Итератор - показывет как правильно создавать пользовательские коллекции.
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
