7caae89e2fca9b71433986ffc1251ef0befccc09
Roman <poma.borodavka@gmail.com>
2018-06-07 23:41:09 +0300

    continue on collections

--- HttpAsyncAwait/UserCollection/Element.cs
+++ HttpAsyncAwait/UserCollection/Element.cs
@@ -9,13 +9,20 @@ namespace UserCollection
     // Экземпляры этого класса будет содержать коллекция UserCollection
     public class Element
     {
-        //Поля.
         private string name;
         private int field1;
         private int field2;
 
+        public Element(string name, int field1, int field2)
+        {
+            this.name = name;
+            this.field1 = field1;
+            this.field2 = field2;
+        }
+
         public string Name { get => name; set => name = value; }
         public int Field1 { get => field1; set => field1 = value; }
         public int Field2 { get => field2; set => field2 = value; }
+
     }
 }
---
+++ HttpAsyncAwait/UserCollection/UserCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCollection
+{
+    //Класс UserCollection коллекция (набор) обьектов класса Element.
+    //Для применения foraech, необходимо, чтобы класс реализовывал интерфейс - IEnumerable.
+    public class UserCollection : IEnumerable, IEnumerator //интерфейс - это контракт
+    {
+        public Element[] elementsArray = null;
+
+        public UserCollection()
+        {
+            elementsArray = new Element[4];
+            elementsArray[0] = new Element("A", 1, 10);
+            elementsArray[1] = new Element("B", 2, 20);
+            elementsArray[2] = new Element("C", 3, 30);
+            elementsArray[3] = new Element("D", 4, 40);
+        }
+        //указатель текущей позиции элемента в массиве.
+        private int position = -1;
+        public IEnumerator GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Current { get; }
+    }
+}
