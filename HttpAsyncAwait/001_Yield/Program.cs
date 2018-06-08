79b177c4a079607349b333b595d9e1e58079a2b5
Roman <poma.borodavka@gmail.com>
2018-06-08 23:50:18 +0300

    finished for today

---
+++ HttpAsyncAwait/001_Yield/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Yield
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable enumrl = UserCollection.Power();
+            foreach (string element in enumrl)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine(new string('-', 30));
+
+            //------------------------------
+            //Так работает foreach
+            IEnumerable enumerable = UserCollection.Power();
+            IEnumerator enumerator = enumerable.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                String element = enumerator.Current as String;
+                Console.WriteLine(element);
+            }
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HttpAsyncAwait/001_Yield/UserCollection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_Yield
+{
+    public class UserCollection
+    {
+        // yield требует специальной нотации - нужно чтобы метод возвращал IEnumerable
+        public static IEnumerable Power() //то, что программа работает - это благодаря одному слову - yield
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                yield return "Hello world! " + i;
+            }
+
+            yield return "The end";
+        }
+
+        //"Волшебное обьяснение".
+        // Первый yield return видит, что он первый и "вешает мешочек" и забрасывает туда значение, к примеру "Hello world! ".
+        // Другие yield`ы видят что мешочек есть и просто забрасывают в него свое значение.
+        // Последний yield видит, что он последний и тогда "завязывает" мешочек и "выбрасывает" его как возвращаемое значение метода Power.
+
+        //yield - оператор генерации программных кодов коллекции!
+
+        //Техническое обьянение.
+    }
+}
