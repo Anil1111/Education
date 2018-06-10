557a261d538d0057897d2386c9bac8bdd995379b
Roman <poma.borodavka@gmail.com>
2018-06-10 23:27:11 +0300

    finished how_to about collections

---
+++ HttpAsyncAwait/IEnumerable_IEnumerator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IEnumerable_IEnumerator
+{
+    class MyCollection : IEnumerable, IEnumerator
+    {
+        private int[] array = { 1, 59, 43, 67, 54, 12, 33 };
+        private int position=-1;
+        public IEnumerator GetEnumerator()
+        {
+            return this;
+        }
+
+        public bool MoveNext()
+        {
+            if (position < array.Length - 1)
+            {
+                position++;
+                return true;
+            }
+            else
+            {
+                Reset();
+                return false;
+            }
+        }
+
+        public void Reset()
+        {
+            this.position = -1;
+        }
+
+        public object Current => array[position];
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyCollection collection = new MyCollection();
+            foreach (var item in collection)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HttpAsyncAwait/yield/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yield
+{
+    class Program
+    {
+        private static List<int> myList = new List<int>();
+
+        static void Fill()
+        {
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(5);
+            myList.Add(6);
+        }
+        static void Main(string[] args)
+        {
+            Fill();
+            foreach (var element in myList)
+            {
+                Console.WriteLine(element);
+            }
+            Console.WriteLine("-".PadRight(50));
+            foreach (var element in Method())
+            {
+                Console.WriteLine(element);
+            }
+
+            //Delay
+            Console.ReadKey();
+        }
+
+        private static IEnumerable Method()
+        {
+            foreach (var element in myList)
+            {
+                if (element > 2)
+                {
+                    yield return element;
+                }
+
+                if (element == 5)
+                {
+                    yield break;
+                }
+            }
+        }
+    }
+}
