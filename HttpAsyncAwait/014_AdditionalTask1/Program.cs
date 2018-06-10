bdd67ce596cd5a5aa2a1a25bbb0b7bc8900f6248
Roman <poma.borodavka@gmail.com>
2018-06-11 00:45:50 +0300

    started doing homework

---
+++ HttpAsyncAwait/014_AdditionalTask1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _014_AdditionalTask1
+{
+    class Program
+    {
+        static IEnumerable GetEvenCollection(int[] array)
+        {
+            foreach (var number in array)
+            {
+                if (number % 2 == 0)
+                {
+                    yield return number; //помним, что yield создает nested class
+                }
+            }
+            //if (array.Length != 0)
+            //{
+            //    for (int i = 0; i < array.Length; i++)
+            //    {
+            //        if (array[i] % 2 == 0)
+            //        {
+            //            yield return array[i];
+            //        }
+            //    }
+            //}
+            //else
+            //{
+            //    yield break;
+            //}
+        }
+        static void Main(string[] args)
+        {
+            var array = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var evenArray = GetEvenCollection(array);
+            foreach (var element in evenArray)
+            {
+                Console.WriteLine(element);
+            }
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HttpAsyncAwait/014_Task1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _014_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> list = new MyList<string>();
+            list.Add("Roma");
+            list.Add("Petya");
+            list.Add("Vasya");
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+
+            var a = list[500];
+            var secondElement = list[2];
+            Console.WriteLine(secondElement);
+
+            var arrayLength = list.Length;
+            Console.WriteLine(arrayLength);
+
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
