97c07c64a513d456e5540e4b0201ae7c65994d03
Roman <poma.borodavka@gmail.com>
2018-06-16 00:32:05 +0300

    started homework

---
+++ HttpAsyncAwait/015_AdditionalTask1/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _015_AdditionalTask1
+{
+    class Calculator
+    {
+        public double Add(double arg1, double ard2) => arg1 + ard2;
+
+        public double Sub(double arg1, double arg2) => arg1 - arg2;
+
+        public double Mul(double arg1, double arg2) => arg1 * arg2;
+
+        public int? Div(int arg1, int arg2)
+        {
+            try
+            {
+                return arg1 / arg2;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Обработка исключения.");
+                Console.WriteLine(e.Message);
+                return null;
+            }
+        }
+    }
+}
---
+++ HttpAsyncAwait/015_AdditionalTask1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.WebSockets;
+
+namespace _015_AdditionalTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите первое число");
+            var number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число");
+            var number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите желаемую операцию: '+' - сложение,\n" +
+                              " '-' - вычетание,\n" +
+                              " '*' - умножение,\n" +
+                              " '/' - деление\n");
+            var operationSign = Console.ReadLine();
+            var calculator = new Calculator();
+            double? result = 0;
+            bool calculation = true;
+            switch (operationSign)
+            {
+                case "+":
+                    result = calculator.Add(number1, number2);
+                    break;
+                case "-":
+                    result = calculator.Sub(number1, number2);
+                    break;
+                case "*":
+                    result = calculator.Mul(number1, number2);
+                    break;
+                case "/":
+                    result = calculator.Div(number1, number2);
+                    break;
+                default:
+                    Console.WriteLine("Неправильный знак операции");
+                    calculation = false;
+                    break;
+            }
+
+            if (calculation && result != null)
+            {
+                Console.WriteLine($"{number1} {operationSign} {number1} = {result}");
+            }
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
