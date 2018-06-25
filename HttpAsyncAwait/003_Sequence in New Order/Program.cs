8e5f29c4ec9ede647f03afe4e65ceb41aec44a67
Roman <poma.borodavka@gmail.com>
2018-06-25 23:32:36 +0300

    continie working on LINQ article

--- HttpAsyncAwait/003_Sequence in New Order/Program.cs
+++ HttpAsyncAwait/003_Sequence in New Order/Program.cs
@@ -8,10 +8,89 @@ namespace _003_Sequence_in_New_Order
 {
     class Program
     {
+        private static string GetLastWord(string words) => words.Substring(words.LastIndexOf(' ') + 1);
+        private static string GetFirstWord(string words) => words.Substring(0, words.IndexOf(' '));
         static void Main(string[] args)
         {
             //Sequence in New Order
             //Some methods return a new sequence where all or a portion of the original sequence is included, but in a different order.
+            //These methods include: Distinct, Except, GroupBy, GroupJoin, Intersect, Join, OrderBy, OrderByDescending, Reverse, ThenBy, ThenByDescending, and Union.
+
+            //Distinct
+            //The Distinct method selects unique items from a sequence. When the same item occurs multiple in a sequence,
+            //only ine instance of the item is included.
+            string[] stooges = { "Moe Howard", "Larry Fine", "Curly Howard", "Moe Howard" };
+            var a = stooges.Distinct(); //returns: Moe Howard, Larry Fine, Curly Howard
+
+            //Except
+            //The Except method gets the unique items from a sequence that are not present in another sequence.
+            string[] citrus = { "grapefruit", "lemon", "lime", "orange" };
+            string[] fruits = { "banana", "banana", "lemon", "lime", "lime" };
+            var b = fruits.Except(citrus); //returns: banana
+
+            //GroupBy
+            //The GroupBy method groups items in a sequence by a unique trait(or key). It Produces a sequence of groopings,
+            //where each is itself a sequence, with associated key value.
+            string[] stoogess = { "Moe Howard", "Larry Fine", "Curly Howard", "Moe Howard" };
+            var c = stoogess.GroupBy(name => GetLastWord(name));
+            // GroupBy: [Key=Howard, Items=Moe Howard, Curly Howard, Moe Howard], [Key=Fine, Items=Larry Fine]
+
+            //GroupJoin !!!
+            //The GroupJoin method os similar to SQL LEFT OUTER JOIN.
+            //var courseFaculty = courses.GroupJoin(faculty, course => course.Id, teacher => teacher.CourseId,
+            //(course, teachers) => new { Course = course.Name, Teachers = teachers });
+
+            //Intersect
+            //The Intersect method gets the distinct items common to two sequences.
+            string[] citrus1 = { "grapefruit", "lemon", "lime", "orange" };
+            string[] fruits1 = { "banana", "banana", "lemon", "lime", "lime" };
+            var d = fruits1.Intersect(citrus1); //returns lemon, lime
+
+            //Join !!!!
+            //The Join method is similar to SQL LEFT INNER JOIN.
+            //var courseTeacher = courses.Join(faculty, course => course.Id, teacher => teacher.CourseId,
+            //(course, teacher) => new { Course = course.Name, Teacher = teacher.Name });
+
+            //OrderBy
+            //The OrderBy method sorts the sequence, in ASCENDING order by a key value obtained from each item in the sequence.
+            string[] stooges3 = { "Moe Howard", "Larry Fine", "Curly Howard", "Moe Howard" };
+            //все равно, что  var e = stooges3.OrderBy(GetFirstWord);
+            //here is a key which helps to order GetFirstWord(name)
+            var e = stooges3.OrderBy(name => GetFirstWord(name)); //returns: Curly Howard, Larry Fine, Moe Howard, Moe Howard
+
+            //OrderByDescending
+            //The OrderBy method sorts the sequence, in ASCENDING order by a key value obtained from each item in the sequence.
+            string[] stooges4 = { "Moe Howard", "Larry Fine", "Curly Howard", "Moe Howard" };
+            //все равно, что  var e = stooges3.OrderByDescending(GetFirstWord);
+            //here is a key which helps to order GetFirstWord(name)
+            var f = stooges3.OrderByDescending(name => GetFirstWord(name)); //returns: Moe Howard, Moe Howard, Larry Fine, Curly Howard
+
+            //Reverse
+            //The Reverce method reverses the order of the sequence
+            string[] stooges5 = { "Moe Howard", "Larry Fine", "Curly Howard", "Moz Howard", "Moa Howard" };
+            var g = stooges5.Reverse(); //returns "Moe Howard", "Curly Howard", "Larry Fine", "Moe Howard"
+
+            //ThenBy
+            //The ThenBy method is somewhat unique in that it can only be applied to an IOrderedEnumerable<T>,
+            //generally returned from one of the following methods: OrderBy, OrderByDescending, ThenBy, or ThenByDescending. 
+            //This method provides the capability to sort a sequence, in ASCENDING order, by an additional key value, also obtained from each item in the sequence.
+            //Т.е если у меня есть одинаковые списки(допустим из 2х слов), то можно сначала по одному слову отсортировать с помощью OrderBy, а потом подругому с помощью ThenBy
+            var z = stooges5.OrderBy(name => GetLastWord(name))
+                .ThenBy(name => GetFirstWord(name)); //ThenBy: Larry Fine, Curly Howard, Moa Howard, Moe Howard,  Moz Howard
+
+            //ThenByDescending
+            //The ThenByDescending method is somewhat unique in that it can only be applied to an IOrderedEnumerable<T>,
+            //generally returned from one of the following methods: OrderBy, OrderByDescending, ThenBy, or ThenByDescending. 
+            //This method provides the capability to sort a sequence, in DESCENDING order, by an additional key value, also obtained from each item in the sequence.
+            //Т.е если у меня есть одинаковые списки(допустим из 2х слов), то можно сначала по одному слову отсортировать с помощью OrderBy, а потом подругому с помощью ThenByDescending
+            var z1 = stooges5.OrderBy(name => GetLastWord(name))
+                .ThenByDescending(name => GetFirstWord(name)); // ThenByDescending: Larry Fine, Moz Howard, Moe Howard, Moa Howard, Curly Howard
+
+            //Union
+            //The Union method gets the distinct items resulting from the combination of two sequences.
+            string[] citrus8 = { "grapefruit", "lemon", "lime", "orange" };
+            string[] fruits8 = { "banana", "banana", "lemon", "lime", "lime" };
+            var t = citrus8.Union(fruits8); //returns grapefruit, lemon, lime, orange, banana
         }
     }
 }
---
+++ HttpAsyncAwait/004_SingletonMethods/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _004_SingletonMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //The singleton methods force immediate materialization of at least a portion of the sequence. 
+            //These methods include: 
+            //Aggregate, All, Any, Average, Contains, Count, ElementAt, ElementAtOrDefault, 
+            //First, FirstOrDefault, Last, LastOrDefault, LongCount, Max, Min, SequenceEqual, 
+            //Single, SingleOrDefault, Sum, ToArray, ToDictionary, ToList, and ToLookup.
+
+            //Aggregate
+            //The Aggregate method accumulates the items in a sequence to return a singleton, aggregate value. 
+            //The code below is just an example. It is a terrible implementation of String.Join, which is far more appropriate if you ever need to simply aggregate strings.
+
+            string[] berries = { "blackberry", "blueberry", "raspberry" };
+            //debug and it will be clear
+            string aggregate = berries.Aggregate((accumulator, item) => accumulator + " " + item); //return: blackberry blueberry raspberry
+
+            var a = string.Join(" ", berries); //The same result
+                                               //Warning: If the sequence has zero items, an exception will occur.If the sequence has only one item, 
+                                               //the first item is simply returned, without ever invoking the method you provide.For all other sequences, 
+                                               //the first invocation of your method will be called with the first two items of the sequence.
+
+            //All
+            //The All method gets a Boolean value indicating if all items in a sequence match the specified condition.
+            //This method stops when it reaches the first item that does not match the specified condition.
+            char[] letters = { 'A', 'B', 'C', 'D' };
+            bool all = letters.All(x => Char.IsLetter(x)); //returns true
+
+            //Any
+            //The Any method gets a Boolean value indicating if any items in a sequence match the specified condition. 
+            //This method stops when it reaches the first item that matches the specified condition.
+            char[] letters1 = { 'A', 'B', 'C', 'D' };
+            bool any = letters1.Any(x => Char.IsLetter(x));
+
+            //Average
+            //The Average method averages all of the imes in the sequence and returns a floating point result.
+            byte[] bytes = { 1, 2, 3, 4 };
+            float average = bytes.Average(x => (float)x);
+
+            //Contains
+            //The Contains method gets a Boolean value indicating if the sequence contains the specified item. 
+            //This method stops on the first item that matches the specified item.
+            char[] letters2 = { 'A', 'B', 'C', 'D' };
+            bool contains = letters2.Contains('C');
+
+            //Count
+            //The Count method gets the number of items in the sequence.
+            // The Length property would be more efficient here
+            byte[] byte4 = { 1, 2, 3, 4 };
+            int count = byte4.Count();
+            Console.WriteLine($"Count: {count}");
+
+            //The Count method can also get the number of items in a sequence that match a specified condition.
+
+            var count2 = byte4.Count(x => x > 3); //returns 1
+                                                  //Warning: IEnumerable<T>.Count method must evaluate every item in the sequence and can be quite resource intensive.
+                                                  //It should not be confused with the ICollection<T>.Count property or the Array.Length property. 
+                                                  //Both of these (most often) simply return a pre - existing count and are(generally) not resource intensive.
+
+            //ElementAt
+            //The ElementAt method gets the item at the specified zero-based index within the sequence.
+            byte[] bytes5 = { 1, 2, 3, 4 };
+            byte elementAt = bytes5.ElementAt(1);
+            Console.WriteLine($"ElementAt: {elementAt}"); //returns 2
+
+            //byte elementAt1 = bytes5.ElementAt(10); // Unhandled Exception: System.ArgumentOutOfRangeException: Index was out of range. 
+                                                    //Must be non-negative and less than the size of the collection.
+
+            //Warning: This should not be confused with indexing functionality provided by arrays and lists.
+            //Unlike true indexing, the ElementAt method must evaluate and skip all items prior to the specified index. 
+            //It is functionally equivalent to the following LINQ: IEnumerable<T>.Skip(index).First().
+
+            //ElementAtOrDefault
+            //The ElementAtOrDefault method gets the item at the specified zero-based index within the sequence, 
+            //if the specified item does not-exist, it returns a default value.
+            byte[] bytes6 = { 1, 2, 3, 4 };
+            byte elementAtOrDefault = bytes6.ElementAt(1);
+            Console.WriteLine($"ElementAtOrDefault: {elementAtOrDefault}"); //returns 2
+
+            elementAtOrDefault = bytes6.ElementAtOrDefault(-1);
+            Console.WriteLine($"ElementAtOrDefault: {elementAtOrDefault}"); //returns 0
+
+            //Warning: This should not be confused with indexing functionality provided by arrays and lists.
+            //Unlike true indexing, the ElementAt method must evaluate and skip all items prior to the specified index. 
+            //It is functionally equivalent to the following LINQ: IEnumerable<T>.Skip(index).FirstOrDefault().
+
+        }
+    }
+}
