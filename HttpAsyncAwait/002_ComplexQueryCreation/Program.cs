60fd8387ccfc1ccafeed5232521b351a5e065682
Roman <poma.borodavka@gmail.com>
2018-06-28 17:36:58 +0300

    finished intensive tutorial about LINQ

---
+++ HttpAsyncAwait/002_ComplexQueryCreation/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace _002_ComplexQueryCreation
+{
+    class Program
+    {
+        private static string[] names = { "Tom", "Dick", "Harry", "Mary" };
+        static void Main(string[] args)
+        {
+            //Построение сложных запросов могут строиться 3мя способами:
+            //1. Последовательно
+            //2. Используя ключевое слово into
+            //3. Используя оболочки для запросов
+
+            //1.Последовательно
+            var filtered = names.Where(x => x.Contains("r"));
+            var ordered = filtered.OrderBy(x => x.Length);
+            var result = ordered.Select(x => x.ToUpper());
+
+            //2. Используя ключевое слово into
+            //into позволяет записать в переменную значение одного запроса и выполнять другой запрос с этой переменной.
+            //Этот запрос будет проходится по списку имен и производить замену всех букв, которые попадают в перечень "[aeiou]" на пустое место
+            IEnumerable<string> query = from name in names
+                                        select Regex.Replace(name, "[aeiou]", "");
+
+            query = from n in query
+                    where n.Length > 2
+                    orderby n
+                    select n;
+            //ВОТ ТО, ЧТО НАПИСАНО ВЫШЕ МОЖНО ПЕРЕПИСАТЬ ИСПОЛЬЗУЯ КЛЮЧЕВОЕ СЛОВО into
+
+            IEnumerable<string> query2 =
+                from name in names
+                select Regex.Replace(name, "[aeiou]", "")
+                into newValue
+                where newValue.Length > 2
+                orderby newValue
+                select newValue;
+
+            IEnumerable<string> query3 = names.Select(name => Regex.Replace(name, "[aeiou]", ""))
+                .Where(newValue => newValue.Length > 2)
+                .OrderBy(newValue => newValue);
+
+            //3. Используя оболочки для запросов
+            //Это точно то же самое, что и сверху, она не несет никаких дополнительных расходов, но выглядит она по другому.
+            //Это не подзапрос! С использованием into просто более эллегантно смотрится.
+            var query4 = from a in
+                    (from b in names
+                     select Regex.Replace(b, "[aeiou]", ""))
+                         where a.Length > 2
+                         orderby a
+                         select a;
+
+            var query5 = (from b in names select Regex.Replace(b, "[aeiou]", "")).Where(a => a.Length > 2)
+                .OrderBy(a => a);
+        }
+    }
+}
---
+++ HttpAsyncAwait/Examples/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples
+{
+    class Program
+    {
+        private static string[] names = { "Tom", "Dick", "Harry", "Mary" };
+        private static string[] forUnion = { "Tom", "Dick", "Harry", "Borry" };
+        static void Main(string[] args)
+        {
+            //3. Объединение
+            //names.Join()
+            //names.GroupJoin()
+
+            //4.Упорядочивание
+
+            //OrderBy
+            //по умолчанию сортирует по увеличению
+            var orderedBy = names.OrderBy(x => x); //returns "Dick", "Harry", "Mary", "Tom"
+
+            //OrderByDescending
+            //по умолчанию сортирует по убыванию
+            var orderByDescending = names.OrderByDescending(x => x);  //returns "Tom", "Mary", "Harry", "Dick" 
+
+            //5.Множества
+
+            //Concat 
+            //Соединяет 2 последовательности в одну.
+            var concat = names.Concat(names); //return  "Tom", "Dick", "Harry", "Mary", "Tom", "Dick", "Harry", "Mary"
+
+            //Union
+            //Делает то же самое, что и Concat, но повторяющиеся элементы не будут включены дважды
+            var union = names.Union(forUnion); //returns  "Tom", "Dick", "Harry", "Mary", "Borry"
+
+            //Intersect
+            //Определяет пересечение
+            var intersect = names.Intersect(forUnion); //return "Tom", "Dick", "Harry"
+
+            //Except
+            //Определяет элементы в одной коллекции, которых нету в другой
+            var except = names.Except(forUnion); //returns  "Mary"
+
+            //6. Экспорт - часто используются тогда, когда не нужно сохранять сам запрос и использовать его повторно.
+
+            //ToArray
+            var array = names.ToArray();
+
+            //ToList
+            var list = names.ToList();
+
+            //ToDictionary
+            var dictionary = names.ToDictionary(x => x, x => true);
+
+            //7. Поэлементные
+
+            //Single
+            //Вовзращает один элемент, который подходит под условие. Если элементов больше - то Single бросает исключение.
+            var single = names.Single(x => x == "Tom"); //retuns "Tom"
+
+            //SingleOrDefalut
+            //Вовзращает один элемент, который подходит под условие или значение по умолчанию.
+            //Если элементов больше - то Single бросает исключение.
+            var singleOrDefault = names.SingleOrDefault(x => x == "Tom"); //retuns "Tom"
+
+            Enumerable.Empty<string>();
+            List<string> list1 = new List<string>();
+            //if (list != null)
+            //{
+            //    foreach (var item in list.Where(/* etc */))
+            //    {
+
+            //    }
+            //}
+            
+            //Нету никаких if
+            foreach (var item in list1.EmptyIfNull().Where(x=>x.Contains("r")))
+            {
+                
+            }
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T> collection)
+        {
+            return collection == null
+                ? Enumerable.Empty<T>()
+                : collection;
+        }
+    }
+}
