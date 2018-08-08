614bd4eafa59d29299823a85010ce02b6aca43e6
Roman1137 <poma.borodavka@gmail.com>
2018-08-08 20:34:51 +0300

    started part about BitArray

---
+++ HttpAsyncAwait/001_BitArray/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_BitArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitArray bits = new BitArray(3); //нету конструктора по умолчанию.
+
+            bits[0] = false;
+            bits[1] = true;
+            bits[2] = false;
+            Console.WriteLine(bits.Length);
+
+            //Для изменения размера коллекции необходимо воспользоваться свойством Length
+            bits.Length = 4;
+            bits[3] = true;
+            Console.WriteLine(bits.Length);
+            Console.WriteLine(bits.Count);
+
+            //Ценность BitArray в том, что он позволяет выполнить логические операции над двумя объектами(смотри след. пример)
+
+            //Delay
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HttpAsyncAwait/002_BitArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _002_BitArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bits1 = new BitArray(3)
+            {
+                [0] = true,
+                [1] = true,
+                [2] = true
+            };
+
+            var bits2 = new BitArray(3);
+            bits2[0] = true;
+            bits2[1] = false;
+            bits2[2] = true;
+
+            //Пришлось влепить так потому, что при выполнении операции, значение bits1 и bits2 меняется(ссылочный тип)
+            
+            var resultBits = (bits1.Clone() as BitArray)?.And(bits2.Clone() as BitArray);
+
+            var resultBits2 = (bits1.Clone() as BitArray)?.Or(bits2.Clone() as BitArray);
+
+            var resultBits3 = (bits1.Clone() as BitArray)?.Xor(bits2.Clone() as BitArray);
+
+            var resultBits4 = (bits1.Clone() as BitArray).Not();
+        }
+    }
+}
