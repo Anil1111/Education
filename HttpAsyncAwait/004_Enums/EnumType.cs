f313a548353c8a0c3e751b44a5f100730adddb69
Roman1137 <poma.borodavka@gmail.com>
2018-08-24 23:28:32 +0300

    Added some experiments durng reading the book

--- HttpAsyncAwait/004_Enums/EnumType.cs
+++ HttpAsyncAwait/004_Enums/EnumType.cs
@@ -7,15 +7,15 @@ namespace _004_Enums
         Zero, // = 0
         One, // = 1
         one = One,
-        Two=2,
+        Two = 2,
         Three, // = 3
         Four, // = 4
-        Five =5, 
+        Five = 5,
         Six = Nine,
         Seven, // = 10
-        Eight =8, //Если не указать значение ЯВНО, то на 14 строке будет ошибка!
-        Nine, 
-        Ten =10,
-        Infinite =255
+        Eight = 8, //Если не указать значение ЯВНО, то на 14 строке будет ошибка!
+        Nine,
+        Ten = 10,
+        Infinite = 255
     }
 }
--- HttpAsyncAwait/012_For/Program.cs
+++ HttpAsyncAwait/012_For/Program.cs
@@ -12,12 +12,47 @@ namespace _012_For
         {
             //Циклическая конструкция - for (цикл со счетчиком)
             //for (Инициализация счетчика итерации; Условие ; Изменение счетчика) {Тело цикла}
-            for (int counter=0; counter<3; counter++)
+            for (int counter = 0; counter < 3; counter++)
             {
                 Console.WriteLine($"Counter = {counter}");
             }
             //counter = 0 // counter не доступен за ПРЕДЕЛАМИ ЦИКЛА for
 
+
+
+            double nominal = 1.0;
+            double sum = 0.0;
+            for (int i = 0; i < 10; i++)
+            {
+                sum += 0.1;
+            }
+
+            const double TOLERANCE = 0.00001;
+            bool equals = Math.Abs(nominal - sum) < TOLERANCE;
+            //------------------------------
+
+            double sum1 = 5000000.02 - 5000000.01;
+            //------------------------------
+
+            int termA = 1000000;
+            int termB = 1000000;
+            int termC = termA * termB / 1000000;
+            //-----------------------
+
+            double a = 10;
+            double b = 10.00000000000000000001;
+            double equals1 = a + b; //20
+
+
+            decimal a1 = 10;
+            decimal b1 = 10.00000000000000000001M;
+            decimal sym = a1 + b1;
+
+
+            bool equals11 = b > (double) b1;
+
+
+            int aaa = 7 / 10;
             //Delay
             Console.ReadKey();
         }
