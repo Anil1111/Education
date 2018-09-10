89c14c74f7c011b257002432cc400a716abe6f91
Roman1137 <poma.borodavka@gmail.com>
2018-09-11 00:08:20 +0300

    started working on 5th lesson

---
+++ HttpAsyncAwait/001_XML/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //System.Xml 
+            //Пространство имен, в котором содержаться классы, необходимые для работы в XML файлами.
+        }
+    }
+}
